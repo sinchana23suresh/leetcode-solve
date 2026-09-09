8b1432cf653b57f26fd627c17f54ec36c29a76eb
Sinchana Suresh <ahanahana23@gmail.com>
2026-09-09 22:14:53 +0530

    Valid Anagram - 2026-09-09T16:44:53.298Z

--- 242-valid-anagram/solution.py
+++ 242-valid-anagram/solution.py
@@ -1,7 +1,15 @@
-// 24 ms | 13.4 MB
+// 15 ms | 12.6 MB
 class Solution(object):
     def isAnagram(self, s, t):
-        return sorted(s) == sorted(t)
+        d = {}
+
+        for char in s:
+            d[char] = d.get(char, 0) + 1
+
+        for char in t:
+            d[char] = d.get(char, 0) - 1
+
+        return all(value == 0 for value in d.values())
 
         
         
