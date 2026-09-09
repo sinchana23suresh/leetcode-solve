4286a2daaaea0cddbc770654354673dd8e28c6db
Sinchana Suresh <ahanahana23@gmail.com>
2026-09-09 22:06:28 +0530

    Valid Anagram - 2026-09-09T16:36:28.252Z

--- 242-valid-anagram/solution.py
+++ 242-valid-anagram/solution.py
@@ -1,23 +1,7 @@
-// 21 ms | 14.7 MB
+// 24 ms | 13.4 MB
 class Solution(object):
     def isAnagram(self, s, t):
-        d1 = {}
-        d2 = {}
-        for i in range(len(s)):
-            if s[i] in d1:
-                d1[s[i]]=d1[s[i]]+1
-            else:
-                d1[s[i]]=1
+        return sorted(s) == sorted(t)
 
-        for i in range(len(t)):
-            if t[i] in d2:
-                d2[t[i]]=d2[t[i]]+1
-            else:
-                d2[t[i]]=1
-
-        if d1==d2:
-            return True
-        else:
-            return False
         
         
