0ec81e9f9da596bdcdffabb4a17e2ae4bf221bb3
Sinchana Suresh <ahanahana23@gmail.com>
2026-09-14 19:16:34 +0530

    Top K Frequent Elements - 2026-09-14T13:46:33.134Z

---
+++ 347-top-k-frequent-elements/solution.py
@@ -0,0 +1,21 @@
+// 8 ms | 15.2 MB
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        c={}
+        for i in range(len(nums)):
+            if nums[i] in c:
+                c[nums[i]]=c[nums[i]]+1
+            else:
+                c[nums[i]]=1
+        m=0
+        for v in c.values():
+            if v>m:
+                m=v
+        ans=[]
+        for s in range(m,0,-1):
+            for num in c:
+                if c[num]==s:
+                    ans.append(num)
+                    if len(ans)==k:
+                        return ans
+        
