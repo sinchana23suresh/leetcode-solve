6d82cb048ac05e8c4fc526bfc1b70356b6017538
Sinchana Suresh <ahanahana23@gmail.com>
2026-09-17 22:58:48 +0530

    Maximum Subarray - 2026-09-17T17:28:47.789Z

--- 53-maximum-subarray/solution.py
+++ 53-maximum-subarray/solution.py
@@ -1,11 +1,16 @@
-// 104 ms | 21.3 MB
+// 41 ms | 21.1 MB
 class Solution(object):
     def maxSubArray(self, nums):
         current = nums[0]
         final = nums[0]
 
         for i in range(1, len(nums)):
-            current = max(nums[i], current + nums[i])
-            final = max(final, current)
+            if current + nums[i] < nums[i]:
+                current = nums[i]
+            else:
+                current = current + nums[i]
+
+            if current > final:
+                final = current
 
         return final
