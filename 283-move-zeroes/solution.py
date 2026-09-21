016d0e5d8213fd3ef942997510908882091e1a23
Sinchana Suresh <ahanahana23@gmail.com>
2026-09-21 20:26:28 +0530

    Move Zeroes - 2026-09-21T14:56:26.420Z

---
+++ 283-move-zeroes/solution.py
@@ -0,0 +1,12 @@
+// 4 ms | 13.7 MB
+class Solution(object):
+    def moveZeroes(self, nums):
+        j=0
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[j]=nums[i]
+                j=j+1
+        for i in range(j,len(nums)):
+            nums[i]=0
+        return nums
+        
